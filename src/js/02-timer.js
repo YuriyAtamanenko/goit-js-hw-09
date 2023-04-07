7d51894ef3785374f5aff692ca13be3512cd36c0
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-04-07 09:57:58 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -65,13 +65,13 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days.
+ 
   const days = Math.floor(ms / day);
-  // Remaining hours
+
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
+
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
+
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   updateTimerFace({ days, hours, minutes, seconds });
   return { days, hours, minutes, seconds };
